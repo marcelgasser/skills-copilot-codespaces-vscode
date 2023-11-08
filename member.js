20b2e117d102a476f370b1f644e516a403ae8597
Marcel Gasser <marcelgasser@users.noreply.github.com>
2023-11-08 19:03:37 +0000

    Fixed skillsMember()

--- member.js
+++ member.js
@@ -1,9 +1,9 @@
-export interface SkillsMember {
-    name: string;
-    age: number;
-    email: string;
-}
-
-export function createSkillsMember(name: string, age: number, email: string): SkillsMember {
-    return { name, age, email };
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills/member.html',
+    scope: {
+      member: '='
+    }
+  };
 }
